78371531cc3a5cb42628ab4337c0bd0859207aea
Prinzent <vincent.dittli@bbv.ch>
2020-06-05 11:00:38 +0200

    Prototyp - Bibliothek
    Uploading the Prototype

---
+++ Bibliothek/Bibliothek.Core/FodyNotifyPropertyChangedBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Bibliothek.Core
+{
+    using System.ComponentModel;
+    using PropertyChanged;
+
+    /// <summary>
+    /// This project uses Fody.PropertyChanged to weave the OnPropertyChanged call into properties. (https://github.com/Fody/PropertyChanged)
+    /// To prevent a specific class from having the notification call injection, use the <see cref="DoNotNotify"/> attribute.
+    /// </summary>
+    public class FodyNotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Bibliothek/Bibliothek/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Bibliothek
+{
+    using System.Windows;
+
+    using Bibliothek.Core;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow()
+            {
+                DataContext = new MainWindowViewModel(new Book("Fairy Hotter", "Science", 651))
+            };
+
+            mainWindow.Show();
+        }
+    }
+}
