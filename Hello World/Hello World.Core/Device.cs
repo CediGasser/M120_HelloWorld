bd8ec0561624c00ecf1513ce3fe87e35c3327d2d
CediGasser <58805568+CediGasser@users.noreply.github.com>
2020-06-09 23:17:17 +0200

    First Classes
    Needs much more work, change UML according to further development of the Model / ModelView

---
+++ Hello World/Hello World.Core/Device.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hello_World.Core
+{
+    class Device : IHelloWorldProducer
+    {
+        public Device(Game game, float baseHWps, string name)
+        {
+            BaseHWps = baseHWps;
+            Name = name;
+            game.NotifyUpgrading += this.OnNotifyUpgrading;
+        }
+
+        public string Name { get; }
+        public int Count { get; set; }
+        public int Prize { get; set; }
+        public float HWps => BaseHWps*Count;
+        public float BaseHWps { get; }
+        public float UpgradeFactor { get; set; }
+
+        public virtual void OnNotifyUpgrading(object sender, IUpgrade upgrade)
+        {
+
+        }
+    }
+
+    class Supercomputer : Device
+    {
+        public Supercomputer(Game game, float baseHWps, string name) : base(game, baseHWps, name)
+        {
+        }
+
+        public override void OnNotifyUpgrading(object sender, IUpgrade upgrade)
+        {
+
+            base.OnNotifyUpgrading(sender, upgrade);
+        }
+    }
+}
