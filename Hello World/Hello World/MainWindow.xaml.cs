d37f50352e1ce31f9693a1cca84d27e18379c941
CediGasser <58805568+CediGasser@users.noreply.github.com>
2020-06-10 17:48:28 +0200

    Made frame work

--- Hello World/Hello World/MainWindow.xaml.cs
+++ Hello World/Hello World/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hello_World.GamePage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Hello_World
         public MainWindow()
         {
             InitializeComponent();
+            frame.NavigationService.Navigate(new GameView());
         }
     }
 }
