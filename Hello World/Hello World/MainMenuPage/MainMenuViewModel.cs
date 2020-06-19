7ff66391f1562131994ff8a5d3b163fda128d3ae
CediGasser <58805568+CediGasser@users.noreply.github.com>
2020-06-19 15:49:59 +0200

    Added ViewModel

---
+++ Hello World/Hello World/MainMenuPage/MainMenuViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hello_World.MainMenuPage
+{
+    class MainMenuViewModel
+    {
+        public void ExitApplication()
+        {
+
+        }
+    }
+}
