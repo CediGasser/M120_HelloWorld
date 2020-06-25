c8d888344f6f98ee02cd8b831443223c62a9f99d
Prinzent <vincent.dittli@bbv.ch>
2020-06-25 23:33:01 +0200

    Add GameViewModel

---
+++ Hello World/Hello World/GamePage/GameViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hello_World.Core;
+
+namespace Hello_World.GamePage
+{
+    class GameViewModel
+    {
+        private readonly Game game;
+    }
+}
