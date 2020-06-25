4506fa0738cb0ce182b20f3c387def4cbbd7a24b
Prinzent <vincent.dittli@bbv.ch>
2020-06-25 23:06:09 +0200

    Edited MainMenuViewModel

--- Hello World/Hello World/MainMenuPage/MainMenuViewModel.cs
+++ Hello World/Hello World/MainMenuPage/MainMenuViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Hello_World.Core;
 
 namespace Hello_World.MainMenuPage
 {
-    class MainMenuViewModel
+    class MainMenuViewModel : FodyNotifyPropertyChangedBase
     {
+        private readonly Game game;
+
+        public MainMenuViewModel(Game game)
+        {
+            this.game = game;
+        }
+
+        public int Karma => game.Karma;
     }
 }
