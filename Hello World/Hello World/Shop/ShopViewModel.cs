c22b4b591ab33f13e4f14e6274545f58e5ca58e0
Prinzent <vincent.dittli@bbv.ch>
2020-06-29 03:17:32 +0200

    Created ShopViewModel

---
+++ Hello World/Hello World/Shop/ShopViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hello_World.Infrastructure.ViewModels;
+
+namespace Hello_World.Shop
+{
+    class ShopViewModel : ViewModelBase
+    {
+    }
+}
